3ae74d55301fa23de9b314ba3669106b5bedff3c
zach.gharst <zdgharst@gmail.com>
2021-03-06 14:34:39 -0600

    Accept input from file

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char* argv[]) {
-    std::cout << "Hello world\n";
+    int compatibilities[40000];
+    std::ifstream compatibilityFile;
+    compatibilityFile.open("data/roommates.txt");
+
+    for(int i = 0; i < 40000; i++) {
+        compatibilityFile >> compatibilities[i];
+    }
+
+    compatibilityFile.close();
+
+    std::cout << compatibilities[203] << "\n";
 
     return 0;
 }
